9ef01fcfa2bfe4a8f9f2560e918b6a2a606de393
ShuddhiStuti <nisharanjeetjaiswal@gmail.com>
2024-09-29 11:20:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+/*
+    Use beginPath() function to begin a path.
+    Then,  use strokeStyle to set the color to "grey".
+    Use lineWidth and set the width to 1.
+    Use rect() to create a rectangle at 150 and 143
+    with 430 width and 200 height.
+    */
+   ctx.beginPath();
+   ctx.strokeStyle="grey";
+   ctx.lineWidth=1;
+   ctx.rect()(150,143,430,200)
+   ctx.stroke();
+
+   
+   /*
+    Use beginPath() function to begin a path.
+    Then,  use strokeStyle to set the color to "blue".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x= 250 and y=210
+    with 40 as radius,0 startAngle and 2*Math.PI endAngle.
+    */
+    ctx.beginPath();
+    ctx.strokeStyle="blue";
+    ctx.lineWidth=5;
+    ctx.arc()(250,210,40,0,2*Math.PI)
+    ctx.stroke();
+
+    /*
+    Use beginPath() function to begin a path.
+    Then,  use strokeStyle to set the color to "black".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x= 350 and y=210
+    with 40 as radius,0 startAngle and 2*Math.PI endAngle.
+    */
+    ctx.beginPath();
+    ctx.strokeStyle="black";
+    ctx.lineWidth=5;
+    ctx.arc()(350,210,40,0,2*Math.PI)
+    ctx.stroke();
+
+    /*
+    Use beginPath() function to begin a path.
+    Then,  use strokeStyle to set the color to "red".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x= 450 and y=210
+    with 40 as radius,0 startAngle and 2*Math.PI endAngle.
+    */
+    ctx.beginPath();
+    ctx.strokeStyle="red";
+    ctx.lineWidth=5;
+    ctx.arc()(450,210,40,0,2*Math.PI)
+    ctx.stroke();
+
+    /*
+    Use beginPath() function to begin a path.
+    Then,  use strokeStyle to set the color to "orange".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x= 300 and y=250
+    with 40 as radius,0 startAngle and 2*Math.PI endAngle.
+    */
+    ctx.beginPath();
+    ctx.strokeStyle="orange";
+    ctx.lineWidth=5;
+    ctx.arc()(300,250,40,0,2*Math.PI)
+    ctx.stroke();
+
+    /*
+    Use beginPath() function to begin a path.
+    Then,  use strokeStyle to set the color to "green".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x= 400 and y=250
+    with 40 as radius,0 startAngle and 2*Math.PI endAngle.
+    */
+    ctx.beginPath();
+    ctx.strokeStyle="green";
+    ctx.lineWidth=5;
+    ctx.arc()(400,250,40,0,2*Math.PI)
+    ctx.stroke();
+
